2d2c9a9a88671f7dc6da8b58a9ea21aa4ea82a8b
Takumi <nase49946@gmail.com>
2024-06-17 17:25:29 +0900

    Validating Params and Throwing Responses

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,10 +1,16 @@
-import { Form, json, useLoaderData } from '@remix-run/react';
 import type { FunctionComponent } from 'react';
+import { Form, json, useLoaderData } from '@remix-run/react';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import invariant from 'tiny-invariant';
 import { getContact } from '../data';
 import type { ContactRecord } from '../data';
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, 'Missing contactId param');
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response('Not Found', { status: 404 });
+  }
   return json({ contact });
 };
 
