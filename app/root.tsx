7e930045ee33fc2b2f3bc155f1eb09ceae00ff58
Takumi <nase49946@gmail.com>
2024-06-19 11:44:45 +0900

    Adding Search Spinner

--- app/root.tsx
+++ app/root.tsx
@@ -36,6 +36,9 @@ export default function App() {
   const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
   const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q');
 
   useEffect(() => {
     const searchField = document.getElementById('q');
@@ -68,8 +71,9 @@ export default function App() {
                 placeholder="Search"
                 type="search"
                 name="q"
+                className={searching ? 'loading' : ''}
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
@@ -107,7 +111,9 @@ export default function App() {
         </div>
         <div
           id="detail"
-          className={navigation.state === 'loading' ? 'loading' : ''}
+          className={
+            navigation.state === 'loading' && !searching ? 'loading' : ''
+          }
         >
           <Outlet />
         </div>
